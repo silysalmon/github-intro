91d234a876288d3bde044cad2c2b771301bd398f
Akash <akashdurai700@gmail.com>
2020-05-27 20:24:39 +0530

    corrections in win

--- win.py
+++ win.py
@@ -1,10 +1,11 @@
 import tkinter
-from PIL import Image
+from PIL import Image,ImageTk
 win = tkinter.Tk()
 win.title("GUI")
 
 im = Image.open('/home/akash/Pictures/Wallpapers/nature-sky-sunset-the-mountains-66997.jpg')
-lab = tkinter.Label(image=im)
+im_tk=ImageTk.Photoimage(im)
+lab = tkinter.Label(image=im_tk)
 lab.grid(row=0,column=0)
 
 win.mainloop()
